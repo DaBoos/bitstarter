4212f609cd780ca82a7ea3b2da3bf4b69fdfbd83
DaBoos <benoit.allibe@gmail.com>
2013-07-08 22:53:59 +0000

    Updated web.js with fs;.readFile and toString test13

--- web.js
+++ web.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var fs = require('fs');
 
+var content;
+    fs.readFile('./index.html', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        content = data;
+    });
+
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(25);
-len = buf.write("Hello toi");
-console.log(buf.toString());
-
-
 app.get('/', function(request, response) {
-  response.send("hell");
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;

0495ab20f9c3906e09e43ed75ac8c3598273143e
DaBoos <benoit.allibe@gmail.com>
2013-07-09 22:53:54 +0000

    after hours of misery...

--- grader.js
+++ grader.js
@@ -21,11 +21,13 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTML_DEFAULT = "http://www.tuteurs.ens.fr/unix/editeurs/emacs_memo.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,13 +38,25 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = function(checksfile) {
+loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
+
+checkHtml = function(html, checks) {
+    $ = cheerio.load(html);
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;    
+};
+
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
@@ -61,14 +75,46 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+//download html from url
+function download(url, callback) {
+    var resp = rest.get(url);
+    resp.on('complete', function(result) {
+        if (result instanceof Error) {
+            // callback(result);
+            sys.puts('Error: ' + result.message);
+            this.retry(5000); // try again after 5 sec
+            return;
+        }
+        callback(null, result);
+    });
+}
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'url of index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    function check(err, html) {
+      if (err) {
+        console.log('Error getting html: ' + err);
+        process.exit(1);
+      }
+      var checks = loadChecks(program.checks);
+      var checkJson = checkHtml(html, checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+    
+    if (program.url){
+        download(program.url, check);
+    } else if (program.url){ 
+	fs.readFile(program.file, check);
+    }
+
 } else {
+    exports.loadChecks = loadChecks;
     exports.checkHtmlFile = checkHtmlFile;
 }
